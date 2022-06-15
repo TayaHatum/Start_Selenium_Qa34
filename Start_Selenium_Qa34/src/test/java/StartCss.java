c1a4847f4171b491e6b89d1a36635b4254f794ad
TayaHatum <21taya21@gmail.com>
2022-06-15 20:08:21 +0300

    add login success

--- Start_Selenium_Qa34/src/test/java/StartCss.java
+++ Start_Selenium_Qa34/src/test/java/StartCss.java
@@ -10,15 +10,15 @@ public class StartCss {
     WebDriver wd;
 
     @Test
-    public void startCssLocators(){
-        wd=new ChromeDriver();
+    public void startCssLocators() {
+        wd = new ChromeDriver();
         wd.navigate().to("https://contacts-app.tobbymarshall815.vercel.app/home");
 
-        WebElement el1 =wd.findElement(By.id("root"));
-        WebElement el2 =wd.findElement(By.tagName("a"));
-        List<WebElement> list =wd.findElements(By.tagName("a"));// ---> 3 a
+        WebElement el1 = wd.findElement(By.id("root"));
+        WebElement el2 = wd.findElement(By.tagName("a"));
+        List<WebElement> list = wd.findElements(By.tagName("a"));// ---> 3 a
         wd.findElement(By.className("container"));
-       // wd.findElement(By.name("name"));
+        // wd.findElement(By.name("name"));
 
         wd.findElement(By.linkText("LOGIN"));
         wd.findElement(By.partialLinkText("LOG"));
@@ -30,11 +30,11 @@ public class StartCss {
         wd.findElement(By.cssSelector("a"));
         wd.findElement(By.cssSelector("input"));
         wd.findElement(By.cssSelector("button"));
- // id ----> #
+        // id ----> #
         wd.findElement(By.cssSelector("#root"));
 // class ---> .class
-wd.findElement(By.cssSelector(".container"));
-wd.findElement(By.cssSelector("[href='/home']"));
+        wd.findElement(By.cssSelector(".container"));
+        wd.findElement(By.cssSelector("[href='/home']"));
 
 
     }
